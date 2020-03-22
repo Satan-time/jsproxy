4b905d58de94b74236a7942d256a098e10d742d0
Satan-time <62492229+Satan-time@users.noreply.github.com>
2020-03-23 00:00:33 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -16,14 +16,14 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
+        [location.host]: 1,
+        [location.host]: 2,
       }
     },
     'demo-sg': {
       label: '演示服务-新加坡节点',
       lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
+       [location.host]: 1,
       },
     },
     'mysite': {
@@ -37,6 +37,7 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
+        [location.host]: 1,
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
 
